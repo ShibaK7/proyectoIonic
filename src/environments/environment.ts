5393fb74260a3f5fb9880eca82c9dad358acef10
ShibaK7 <shirou.k7@gmail.com>
2020-01-10 13:05:41 -0600

    generar base del proyecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    BACKENDLESS: {
+        APP_ID: '0AB47AD0-AD72-46F0-AF6B-EF79197ECD3A',
+        API_KEY: '0E05E82A-8F12-41D4-89A3-A85EAD7E6158'
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
